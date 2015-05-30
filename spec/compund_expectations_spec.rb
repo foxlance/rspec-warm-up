fd30fff2a2826331f7df84c362e87fd8c644ce5e
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-05-30 08:46:47 +0000

    Compound matchers

---
+++ spec/compund_expectations_spec.rb
@@ -0,0 +1,12 @@
+fdescribe "Compund Matchers" do
+    
+    it "Allows for multiple expectations" do
+        string = "lance"
+
+        expect(string).to start_with("l").and end_with("e") # multiple conditions
+        expect(string).to start_with("l").or end_with("e") # multiple conditions
+        expect(20).to be_between(1,20) #be_between().exclusive
+
+    end
+    
+end
