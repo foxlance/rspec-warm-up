84d68c0cc21c9c86e20ec5506734d79e100f859d
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-05-31 08:18:49 +0000

    Skipping test

---
+++ spec/skip_test_spec.rb
@@ -0,0 +1,23 @@
+describe "Matchers 101" do
+    
+    # appending 'x' on a test case skips its execution
+    xit "Assertions on equality" do
+        number = 3
+        expect(number).to eq 3
+    end
+
+    # skip is a more verbose way of skipping a test case
+    it "Assertions on equality", skip: 'Not important for now' do
+        number = 3
+        expect(number).to eq 3
+    end
+    
+    # 'pending' has been remade to instead raise a failure even on passing test
+    #  this is great to use as 'mark this one' usage, e.g. future review
+    it "Assertions on equality" do
+        pending
+        number = 3
+        expect(number).to eq 3
+    end
+
+end
