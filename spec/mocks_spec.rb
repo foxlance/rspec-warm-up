b5493ae783c89ae66fac555fc7d1011f2a5b08c2
Lance Vincent <lancevincent@gmail.com>
2015-06-10 20:04:18 +0800

    more mock samples

--- spec/mocks_spec.rb
+++ spec/mocks_spec.rb
@@ -1,6 +1,6 @@
 require "Book"
 
-describe "Mocks" do
+fdescribe "Mocks" do
 
     # mocks behaves like a stub but with a different purpose
     it "Testing mocked object" do
@@ -23,6 +23,19 @@ describe "Mocks" do
         # uncommenting the following line will fail the test as 
         # it did not meet the expectation you set in line 21
         book_mock.borrow
+    end
+
+    it "Expect multiple parameters over the course of test" do
+        book_mock = double()
+
+        # This line means that over the test course, book_mock is allowed to access the following attributes and values
+        # allow(book_mock).to receive_messages(:title => 'Adventures of Huckleberry Finn', :isbn => 1234554321)
+
+        # Using expect() though means that both attributes should be satisfied over the test course in order to pass
+        expect(book_mock).to receive_messages(:title => 'Adventures of Huckleberry Finn', :isbn => 1234554321)
+
+        expect(book_mock.title).to eq('Adventures of Huckleberry Finn')
+        expect(book_mock.isbn).to eq(1234554321)
     end    
 
 end
